10cef863471fd87a4852f3158f0d4d53937d1e36
DESKTOP-O4PAIOJ\moham <mohamedabdelwahabsayed@gmail.com>
2021-09-10 21:16:13 +0200

    resolve on stop crash

--- app/src/main/java/com/example/assignmenttask/service/FileProgressReceiver.kt
+++ app/src/main/java/com/example/assignmenttask/service/FileProgressReceiver.kt
@@ -1,12 +1,10 @@
 package com.example.assignmenttask.service
 
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.example.assignmenttask.R
-import com.example.assignmenttask.ui.MainActivity
 import com.example.assignmenttask.utils.NotificationHelper
 import java.util.*
 
@@ -14,40 +12,42 @@ val TITTLE = "title"
 val ACTION_CLEAR_NOTIFICATION = "ACTION_CLEAR_NOTIFICATION"
 val ACTION_PROGRESS_NOTIFICATION = "ACTION_PROGRESS_NOTIFICATION"
 val ACTION_Downloaded = "ACTION_DOWNLOADED"
-class FileProgressReceiver : BroadcastReceiver() {
-  private val TAG = "FileProgressReceiver"
-  var mNotificationHelper: NotificationHelper? = null
- // val NOTIFICATION_ID = 1
-  var notification: NotificationCompat.Builder? = null
-  lateinit var title: String
-  override fun onReceive(context: Context?, intent: Intent?) {
-    mNotificationHelper = NotificationHelper(context!!)
-    // Get notification id
-    // Get notification id
-    val notificationId = intent!!.getIntExtra("notificationId", 1)
-    title = intent.getStringExtra(TITTLE) ?: ""
-    // Receive progress
-    // Receive progress
-    val progress = intent.getIntExtra("progress", 0)
 
-    when (Objects.requireNonNull(intent.action)) {
-      ACTION_PROGRESS_NOTIFICATION -> {
-        notification = mNotificationHelper?.getNotification(
-          title,
-          context.getString(R.string.in_progress), progress
-        )
-        mNotificationHelper?.notify(notificationId, notification!!)
-      }
-      ACTION_CLEAR_NOTIFICATION -> {
-        mNotificationHelper?.cancelNotification(notificationId)
-      }
-      ACTION_Downloaded -> {
-        notification = mNotificationHelper!!.getNotification(
-          title,
-          context.getString(R.string.file_upload_successful), null
-        )
-        mNotificationHelper!!.notify(notificationId, notification!!)
-      }
+class FileProgressReceiver : BroadcastReceiver() {
+    private val TAG = "FileProgressReceiver"
+    var mNotificationHelper: NotificationHelper? = null
+
+    // val NOTIFICATION_ID = 1
+    var notification: NotificationCompat.Builder? = null
+    lateinit var title: String
+    override fun onReceive(context: Context?, intent: Intent?) {
+        mNotificationHelper = NotificationHelper(context!!)
+        // Get notification id
+        // Get notification id
+        val notificationId = intent!!.getIntExtra("notificationId", 1)
+        title = intent.getStringExtra(TITTLE) ?: ""
+        // Receive progress
+        // Receive progress
+        val progress = intent.getIntExtra("progress", 0)
+
+        when (Objects.requireNonNull(intent.action)) {
+            ACTION_PROGRESS_NOTIFICATION -> {
+                notification = mNotificationHelper?.getNotification(
+                    title,
+                    context.getString(R.string.in_progress), progress
+                )
+                mNotificationHelper?.notify(notificationId, notification!!)
+            }
+            ACTION_CLEAR_NOTIFICATION -> {
+                mNotificationHelper?.cancelNotification(notificationId)
+            }
+            ACTION_Downloaded -> {
+                notification = mNotificationHelper!!.getNotification(
+                    title,
+                    context.getString(R.string.file_upload_successful), null
+                )
+                mNotificationHelper!!.notify(notificationId, notification!!)
+            }
+        }
     }
-  }
 }
